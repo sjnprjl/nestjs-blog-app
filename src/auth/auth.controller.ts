2528dbc413db7b87aa01dea24eff1223ec92b12e
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-16 10:12:15 +0545

    :recycle: refactor: auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,24 +7,24 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
-@UseInterceptors(ClassSerializerInterceptor)
-@SerializeOptions({
-  excludeExtraneousValues: false,
-})
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(private readonly authService: AuthService) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
+  @SerializeOptions({
+    excludeExtraneousValues: false,
+  })
   @Post('/register')
   register(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+    return this.authService.register(createUserDto);
   }
 
   @Post('/login')
-  login(@Body() { usernameOrEmail, password }: LoginDto) {
-    return this.userService.verifyUser(usernameOrEmail, password);
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { JWT } from 'config';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    return await this.usersService.create(createUserDto);
+  }
+
+  async login({ usernameOrEmail, password }: LoginDto) {
+    const user = await this.usersService.verifyUser(usernameOrEmail, password);
+    const claims: any = { sub: user.id, email: user.email };
+
+    const accessToken = await this.jwtService.signAsync(claims, {
+      expiresIn: '7d',
+      secret: JWT.secretOrKey,
+    });
+
+    return {
+      jwt: accessToken,
+    };
+  }
+}
