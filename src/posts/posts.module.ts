16addbfb26cc2399c477357ab66fb3c0c72077a1
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-17 01:05:09 +0545

    :recycle: export UsersService

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -9,5 +9,6 @@ import { PostsService } from './posts.service';
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
   providers: [PostsService],
+  exports: [PostsService],
 })
 export class PostsModule {}
