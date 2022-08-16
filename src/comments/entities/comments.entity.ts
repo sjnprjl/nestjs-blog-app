7128b579d6bb17d54573c1de5c0de89c8c6b3a35
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-16 22:48:42 +0545

    :recycle: add exclude decorator

--- src/comments/entities/comments.entity.ts
+++ src/comments/entities/comments.entity.ts
@@ -1,10 +1,11 @@
+import { Exclude } from 'class-transformer';
+import { Post } from 'src/posts/entities/posts.entity';
 import { User } from 'src/users/entities/users.entity';
 import {
   Column,
   Entity,
   JoinColumn,
   ManyToOne,
-  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -19,19 +20,27 @@ export class PostComment implements IComment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Exclude()
+  @Column({ nullable: true })
   userId: string;
 
   @Column({ nullable: true })
   parentCommentId: string;
 
-  @OneToOne(() => PostComment, { nullable: true, onDelete: 'CASCADE' })
+  @Column()
+  postId: string;
+
+  @ManyToOne(() => PostComment, { nullable: true, onDelete: 'CASCADE' })
   @JoinColumn({ name: 'parentCommentId' })
   parentComment: IComment;
 
-  @ManyToOne(() => User, (user) => user.comments)
+  @ManyToOne(() => User, (user) => user.comments, { onDelete: 'SET NULL' })
   @JoinColumn({ name: 'userId' })
   user: User;
+
+  @ManyToOne(() => Post, (post) => post.comments, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  post: Post;
 
   @Column()
   comment: string;
