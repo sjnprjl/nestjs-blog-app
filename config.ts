dac056196d70648ac5d4ee149e33800362a78a60
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-16 10:10:31 +0545

    :sparkles: add: jwt interface

--- config.ts
+++ config.ts
@@ -8,10 +8,18 @@ interface IDatabase {
   username: string;
 }
 
+interface IJwtSecret {
+  secretOrKey: string;
+}
+
 export const DB: IDatabase = {
   database: process.env.DB_NAME,
   port: +process.env.DB_PORT,
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   username: process.env.DB_USERNAME,
+};
+
+export const JWT: IJwtSecret = {
+  secretOrKey: process.env.JWT_SECRET,
 };
