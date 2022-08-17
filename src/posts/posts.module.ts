0bdf87d305383f529a7c21f624c4b1595691d421
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-18 01:22:48 +0545

    import friends module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FriendsModule } from 'src/users/friends/friends.module';
 import { Post } from './entities/posts.entity';
 
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), FriendsModule],
   controllers: [PostsController],
   providers: [PostsService],
   exports: [PostsService],
