7e3cad2ef91dd328b650e53083bf2e7c6d41ef93
xSjnxyz <mailme.suzan77@gmail.com>
2022-08-21 22:46:36 +0545

    remove: non-needed log.

--- src/feeds/feeds.service.ts
+++ src/feeds/feeds.service.ts
@@ -6,7 +6,6 @@ export class FeedsService {
   constructor(private readonly postService: PostsService) {}
 
   async getFeeds(userId: string) {
-    console.log('fresh hit.');
     return await this.postService.getPostOfFriends(userId);
   }
 }
